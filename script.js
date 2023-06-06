f16e9bc7f763d825943ee6c3eae237b483c577d1
Mathilde <131172286+m-mathilde34@users.noreply.github.com>
2023-06-06 13:15:44 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+
+
+function showAlert() {
+  alert("Page under construction!");
+}
+
+function messageSent() {
+  alert("Thank you for your message. It has now been sent to my humans for review. ♥ - Charlie")
+}
+
